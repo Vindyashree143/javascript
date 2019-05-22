3d1f50f0761832c45c4ff8e31450fb7818f06579
vindya <vkumarostb1@gmail.com>
2019-05-22 14:18:31 +0530

    first commit

---
+++ task2/first/second.js
@@ -0,0 +1,14 @@
+
+var marksHeight=4.5;
+var marksMass=100;
+var marksMass01=mass/(height * height);
+ 
+ 
+ var johnHeight=3.5;
+ var johnMass=90;
+ var johnMass001=mass/(height * height);
+ if(marksMass01>johnMass001) {
+ 	console.log(boolean);
+
+ }
+
